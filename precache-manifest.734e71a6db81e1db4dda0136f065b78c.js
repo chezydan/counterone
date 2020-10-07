c59507164b319e7c52e479de2fff8ac8acb5d47d
dan <chezydan@gmail.com>
2020-10-08 01:20:12 +0300

    Updates

---
+++ precache-manifest.734e71a6db81e1db4dda0136f065b78c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab984db929df44c58ccc191ab4f212e6",
+    "url": "/counterone/index.html"
+  },
+  {
+    "revision": "a51ae6c536612dac6e13",
+    "url": "/counterone/static/css/main.7ceaf946.chunk.css"
+  },
+  {
+    "revision": "336e88342a5b0acac54f",
+    "url": "/counterone/static/js/2.e02d4dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counterone/static/js/2.e02d4dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a51ae6c536612dac6e13",
+    "url": "/counterone/static/js/main.c64ebeb2.chunk.js"
+  },
+  {
+    "revision": "37dd7221c2f4bfbb35d9",
+    "url": "/counterone/static/js/runtime-main.ceb50415.js"
+  }
+]);
